1a1ff628a1810edb54ca8964ae5b033d1c45de6f
salmanhaider-cloud <salmanhaider-cloud@users.noreply.github.com>
2025-09-06 22:14:23 +0500

    Initial commit - CDK ECS Fargate project

--- lib/cdk-ecs-fargate-stack.ts
+++ lib/cdk-ecs-fargate-stack.ts
@@ -1,16 +1,40 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 
 export class CdkEcsFargateStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // VPC
+    const vpc = new ec2.Vpc(this, 'MyVpc', { maxAzs: 2 });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkEcsFargateQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // ECS Cluster
+    const cluster = new ecs.Cluster(this, 'MyCluster', { vpc });
+
+    // Fargate Service + Load Balancer
+    const fargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'MyFargateService', {
+      cluster,
+      cpu: 256,
+      desiredCount: 1,
+      memoryLimitMiB: 512,
+      publicLoadBalancer: true,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromRegistry("nginx"), // ✅ Public image
+        containerPort: 80,
+      },
+    });
+
+    // CloudFront Distribution
+    new cloudfront.Distribution(this, 'MyCFDistribution', {
+      defaultBehavior: {
+        origin: new origins.LoadBalancerV2Origin(fargateService.loadBalancer, {
+          protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
+        }),
+      },
+    });
   }
 }
