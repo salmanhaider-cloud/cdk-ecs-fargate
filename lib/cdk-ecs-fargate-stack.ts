cc0a840e9e28727bcb96c475aba7da2e755664cb
salmanhaider-cloud <salmanhaider-cloud@users.noreply.github.com>
2025-09-08 04:41:38 +0500

    Add CloudFront integration to Fargate stack

--- lib/cdk-ecs-fargate-stack.ts
+++ lib/cdk-ecs-fargate-stack.ts
@@ -4,19 +4,19 @@ import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
-import * as path from 'path'; // ✅ Add path import
+import * as path from 'path';
 
 export class CdkEcsFargateStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // VPC
+    // -------------------- VPC --------------------
     const vpc = new ec2.Vpc(this, 'MyVpc', { maxAzs: 2 });
 
-    // ECS Cluster
+    // -------------------- ECS Cluster --------------------
     const cluster = new ecs.Cluster(this, 'MyCluster', { vpc });
 
-    // Fargate Service + Load Balancer
+    // -------------------- Fargate Service + ALB --------------------
     const fargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'MyFargateService', {
       cluster,
       cpu: 256,
@@ -24,19 +24,24 @@ export class CdkEcsFargateStack extends cdk.Stack {
       memoryLimitMiB: 512,
       publicLoadBalancer: true,
       taskImageOptions: {
-        // ✅ Use absolute path so GitHub Actions always finds Dockerfile
         image: ecs.ContainerImage.fromAsset(path.resolve(__dirname, '..')), 
         containerPort: 80,
       },
     });
 
-    // CloudFront Distribution
-    new cloudfront.Distribution(this, 'MyCFDistribution', {
+    // -------------------- CloudFront Distribution --------------------
+    const distribution = new cloudfront.Distribution(this, 'MyCFDistribution', {
       defaultBehavior: {
         origin: new origins.LoadBalancerV2Origin(fargateService.loadBalancer, {
           protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
         }),
       },
+    });
+
+    // -------------------- Output CloudFront URL --------------------
+    new cdk.CfnOutput(this, 'CloudFrontURL', {
+      value: distribution.distributionDomainName,
+      description: 'Global URL for your Fargate app via CloudFront',
     });
   }
 }
